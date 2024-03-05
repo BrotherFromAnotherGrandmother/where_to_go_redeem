fd699c6c16ae169a645ee3be0c0394338c931f42
BrotherFromAnotherMother <aleksandr.nidodin@gmail.com>
2024-03-05 12:38:56 +0300

    Add model Image

--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -10,3 +10,12 @@ class Place(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    number = models.IntegerField()
+    image = models.ImageField()
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.number) + ' ' + str(self.place)
